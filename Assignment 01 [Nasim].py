23081e4f127a497c7e1097ea1504f06a1e171ae9
Natib Hub <122774201+mnasimofficial@users.noreply.github.com>
2023-01-16 13:31:02 +0600

    Add files via upload

---
+++ Assignment 01 [Nasim].py
@@ -0,0 +1,83 @@
+#Print Name:
+
+print("Md Nasim")
+print("AN Basar")
+print("Rashedul Alam Shakil")
+print("Sohan Khan")
+
+#Input your batch number using input() function:
+
+input_name_function = int(input("Enter Your Batch Number: "))
+print(input_name_function)
+
+
+#Create 5 different valid Variables and print:
+
+school = "Rani Bilash Moni High School"
+print(school)
+
+Name = "My Name is Nasim"
+print(Name)
+
+mobileNumber = "01913859148"
+print(mobileNumber)
+
+BATCH = "My Batch No is Web Development Batch 01"
+print(BATCH)
+
+teacher_name = "My teacher name is AN Basar"
+print(teacher_name)
+
+
+#Create Pascal Case Variable:
+
+PASCALE = "tHIS IS PASCALE TYPE VARIABLE"
+print(PASCALE)
+
+
+#Assign a single value to Multiple variables with memory location:
+
+a = b = c = 11
+print(a)
+print(id(a))
+print(b)
+print(id(a))
+print(c)
+print(id(a))
+
+
+#Input float value using input():
+
+float_value = float(input("Enter float value:"))
+print(float_value)
+
+
+#a = ‘Welcome to Django for web and Ai’ convert into upper case:
+
+a = "Welcome to Django for web and Ai"
+print(a.upper())
+
+
+#b = ‘ Python’ remove whitespace:
+
+b = "       Python"
+print(b.strip())
+
+
+#c = 60, d= 22 ‘Conversion int to float’:
+
+c = 60
+d = 22
+
+print(float(c))
+print(float(d))
+
+
+#Create a dictionary & print your info:
+
+dict_example = {
+    "Name": "Nasim, An Basar, Shakil, Sohan",
+    "Varsity": "South, Daffodil, FAU, Daffodil",
+}
+
+print(dict_example)
